ec28e4f58b7d4195a042b9c961adb8f50131f313
GreenMarioX <91627412+GreenMarioX@users.noreply.github.com>
2022-02-12 17:47:03 -0500

    Add files via upload

--- deviationcalculator.py
+++ deviationcalculator.py
@@ -2,20 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-def sample(sigma, slant=0.015):
+
+def sample(sigma, slant=0.05, start=0, end=365, h=1):
     rng = np.random.default_rng()
-    start = 0
-    end = 1
-    h = 0.01
-    steps = int((end-start)/h+1)
+
+    steps = int((end - start) / h + 1)
     mu = 1 + slant
 
     x = np.linspace(start, end, steps)
     y = [1]
-    dis =  rng.normal(mu,sigma,steps-1)
-    
-    for ii in range(len(x)-1):
-        y.append(y[ii]*(dis[ii]))
+    dis = rng.normal(mu, sigma, steps - 1)
+
+    for ii in range(len(x) - 1):
+        if (y[ii] > 0):
+            q = y[ii] * dis[ii]
+        else:
+            q = slant
+
+        if (q > 0):
+            y.append(q)
+        else:
+            y.append(0)
 
     return x, y
 
@@ -44,22 +51,38 @@ def stdDevCalc(values):
 
 def timeFramesDeviation(begin, end, xGraph, yGraph):
     values = []
-    for i in range(len(xGraph)):
-        if xGraph[i] >= begin and xGraph[i] <= end:
-            values.append(yGraph[i])
+    begin = int(begin)
+    end = int(end)
+    for i in range(begin, end):
+        values.append(yGraph[i])
     return stdDevCalc(values)
 
+def weekVolatility(xGraph, yGraph):
+    return timeFramesDeviation(len(xGraph) - 7, len(xGraph), xGraph, yGraph)
+
+def monthVolatility(xGraph, yGraph):
+    return timeFramesDeviation(len(xGraph) - 30, len(xGraph), xGraph, yGraph)
+
+def yearVolatility(xGraph, yGraph):
+    return timeFramesDeviation(len(xGraph) - 180, len(xGraph), xGraph, yGraph)
 
 #Test that looks at the data. Not necessary
-sd = .2
+sd = .3
 err = 0
 sde = 0
-for ii in range(10):
+for ii in range(1):
     xs, ys = sample(sd)
-    sde = timeFramesDeviation(0.5, 0.8, xs, ys)
-    print(sde)
-    err = abs(sd - sde) / sd * 100
-    print(err)
+    sde = timeFramesDeviation(0, len(xs) - 1, xs, ys)
+
+
+
     plt.plot(xs, ys)
     plt.plot(xs[1:-1], movingAvgCalc(ys))
+
+
+
+    print("Standard Deviation: ", sde)
+    print("Week Volatility: ", weekVolatility(xs, ys))
+    print("Month Volatility: ", monthVolatility(xs, ys))
+    print("Year Volatility: ", yearVolatility(xs, ys))
 plt.show()
