08e791012927426fce227395104886f48777aa63
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2022-02-12 23:12:07 -0500

    last of this before I replace the polyfit with curve fitting

--- predict.py
+++ predict.py
@@ -2,60 +2,74 @@ import samplegen as sg #"import FILENAME" will import from the local file
 import matplotlib.pyplot as plt
 import numpy as np
 
-def predict(x,y,length=4):
-    rough, rc = np.polyfit(x[-100:-4:4],y[-100:-4:4],2,cov=True)
-    med, mc = np.polyfit(x[-60:-4:2],y[-60:-4:2],2,cov=True)
-    fine, fc = np.polyfit(x[-40:-4],y[-40:-4],2,cov=True)
+def predict(x,y,length=4,deg=1):
+    hist = len(x)
+    hop = int(len(x)/30)
+
+    rough, rc = np.polyfit(x[::hop],y[::hop],deg,cov=True)
+    med, mc = np.polyfit(x[-90::int(90/30)],y[-90::int(90/30)],deg,cov=True)
+    fine, fc = np.polyfit(x[-30:],y[-30:],deg,cov=True)
+    near, nc = np.polyfit(x[-7:],y[-7:],deg,cov=True)
 
     rc = np.sqrt(np.diag(rc))
     mc = np.sqrt(np.diag(mc))
     fc = np.sqrt(np.diag(fc))
+    nc = np.sqrt(np.diag(nc))
+
+    print(rough)
+    print(med)
+    print(fine)
+    print(near)
 
     a=[]
     ac = []
-    for i in range(len(rough)):
+    for i in range(deg+1):
         a.append(0)
         ac.append(0)
-
-    for i in range(len(rough)):
         a[i]=0
-        a[i]+=rough[i]*rc[i]
-        a[i]+=med[i]*mc[i]
-        a[i]+=fine[i]*fc[i]
-        a[i]/=(rc[i]+mc[i]+fc[i])
-        ac[i]=np.sqrt(mc[i]**2+rc[i]**2+fc[i]**2)
+        a[i]+=rough[i]*rc[i]*(2/3)**2*(1/3)
+        a[i]+=med[i]*mc[i]*(2/3)**3
+        a[i]+=fine[i]*fc[i]*(2/3)**2
+        a[i]+=near[i]*nc[i]*2/3
+        a[i]/=(rc[i]*(2/3)**2*1/3+mc[i]*(2/3)**3+fc[i]*(2/3)**2+nc[i]*2/3)
+        ac[i]=np.sqrt(rc[i]**2+mc[i]**2+fc[i]**2+nc[i]**2)
+    print(ac)
+    print(a)
 
+#Make the actual prediction
     ypre = []
-    for i in range(length, 0, -1):
-        ypr = y[-length]
-        for j in range(1,len(rough)):
-            ypr += a[j]*(x[-length]**j-x[-i]**j)
+    for i in range(length):
+        ypr = y[-1]
+        for j in range(1,deg+1):
+            ypr += a[j]*(x[-1]**j-(x[-1]+i)**j)
         if (ypr < 0):
             ypr =0
         ypre.append(ypr)
 
     #uncertainties
-    yerr = []
     yhigh = []
     ylow = []
-    for i in range(length,0,-1):
+    for i in range(length):
         drift = 0
-        for j in range(1,len(rough)):
-            drift += ac[j]*(x[-length]**j-x[-i]**j)
-        yerr.append(abs(drift))
-        yhigh.append(ypre[-i]+yerr[length-i])
-        if(ypre[-i]-yerr[length-i]<0):
+        for j in range(1,deg+1):
+            drift += ac[j]*(x[-1]**j-(x[-1]+i)**j)
+        yhigh.append(ypre[i]+abs(drift))
+        print(drift)
+        if(ypre[i]-abs(drift)<0):
             ylow.append(0)
         else:
-            ylow.append(ypre[-i]-yerr[length-i])
+            ylow.append(ypre[i]-abs(drift))
 
     return ypre, yhigh, ylow
 
-for i in range(5):
+while(True):
+    d = int(input())
+    if(d==0):
+        quit()
     xs, ys = sg.sample(.3)
-    ypred, yhi, ylo = predict(xs, ys)
-#    plt.plot(xs,ys)
-#    plt.show()
+    ypred, yhi, ylo = predict(xs[:-3], ys[:-3],deg=d)
+    plt.plot(xs,ys)
+    plt.show()
     plt.plot(xs[-7:],ys[-7:])
     plt.plot(xs[-4:],ypred,color='red')
     plt.fill_between(xs[-4:],yhi,ylo,color = 'red',alpha=0.2)
