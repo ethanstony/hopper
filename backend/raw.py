56a2204205cf0b1f9d09e79fe849bbc5eebae76b
thepurplesnail <vanessaman446@gmail.com>
2022-02-12 21:32:37 -0500

    fixed comments & changed startDate to t - 365 days

--- backend/raw.py
+++ backend/raw.py
@@ -13,19 +13,24 @@ def sendRaw():
     pd.set_option("display.max_rows", None, "display.max_columns", None)
 
     #get start date (1 yr before currentDate)
-    startDate = datetime.today() - relativedelta(days=5)
+    startDate = datetime.today() - relativedelta(days=365)
 
     #get current date in yyyy-mm-dd format
     currentDate = time.strftime("%Y-%m-%d")
 
+    #get AAPL data from yahoofinance 
     aapl_df = yf.download('AAPL', 
                         start = startDate,
                         end=currentDate
     )
 
+    #convert fetched dataframe to json
     applson = DataFrame.to_json(aapl_df, orient = "table")
+
+    #convert applson json var to dict
     pythonObj = json.loads(applson)['data']
-    print(pythonObj)
+    
+    #create res dict with subdictionaries that have only 'Date' and 'Close' keys
     res = {}
     resIndex = 0
     for i in pythonObj:
@@ -34,6 +39,6 @@ def sendRaw():
         resObj['Close'] = i['Close']
         res[resIndex] = resObj
         resIndex += 1
+
     print(res)
     return res
-sendRaw()
